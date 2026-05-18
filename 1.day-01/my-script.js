45b63871bbe26f5148147a006621361f952ff15b
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-05-18 18:45:52 +0545

    basics ts code

---
+++ 1.day-01/my-script.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 3));
+// console.log(add('piyush','12'))
+var result = add(10, 2);
+console.log(result);
---
+++ 1.day-01/my-script.ts
@@ -0,0 +1,7 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+console.log(add(2, 3));
+// console.log(add('piyush','12'))
+const result = add(10, 2);
+console.log(result);
